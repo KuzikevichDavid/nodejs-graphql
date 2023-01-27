028d61ed33d73d7cb693247d4be718b3fe000fa4
Davyd <wliorjroo@gmail.com>
2023-01-27 23:25:16 +0300

    feat: add graphql variables

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -3,9 +3,9 @@ import { graphql } from 'graphql';
 import { createSchema, graphqlBodySchema } from './schema';
 
 const plugin: FastifyPluginAsyncJsonSchemaToTs = async (fastify): Promise<void> => {
-  const graphqlSchema = createSchema(fastify.db);
-  const resolveGraphql = (source: string) =>
-    graphql({ schema: graphqlSchema, source: source });
+    const graphqlSchema = createSchema(fastify.db);
+    const resolveGraphql = (source: string, variables: { [x: string]: unknown } | undefined) =>
+        graphql({ schema: graphqlSchema, source: source, variableValues: variables });
 
   fastify.post(
     '/',
@@ -14,9 +14,9 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (fastify): Promise<void> 
         body: graphqlBodySchema
       }
     },
-    async function (request, reply) {
-      if (request.body?.query) return resolveGraphql(request.body?.query);
-      if (request.body?.mutation) return resolveGraphql(request.body?.mutation);
+      async function (request, reply) {
+      if (request.body?.query) return resolveGraphql(request.body?.query, request.body?.variables);
+      if (request.body?.mutation) return resolveGraphql(request.body?.mutation, request.body?.variables);
       return reply.badRequest();
     }
   );
